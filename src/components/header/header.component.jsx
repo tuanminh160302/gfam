1250b54fee83b9fcc896ff6c4eb45cb2abca404c
tuanminh160302 <canhtuan09@gmail.com>
2021-12-06 20:12:19 -0500

    newsfeed basic layout

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import './header.styles.scss'
 
 import UserAvt from '../../assets/media/user-avt.jpg';
-import {ReactComponent as LogOutBtn} from '../../assets/media/power.svg';
+import { ReactComponent as LogOutBtn } from '../../assets/media/power.svg';
 
 import Button from '../button/button.component';
 import { getAuth, signOut } from "firebase/auth";
@@ -11,7 +11,7 @@ import { connect } from 'react-redux';
 
 import { getInputValue } from '../../redux/signInData/signInData.actions';
 
-const Header = ({isSignedIn, setData}) => {
+const Header = ({ isSignedIn, setData }) => {
 
     const [toggleUserNav, setToggleUserNav] = useState(false);
 
@@ -22,45 +22,47 @@ const Header = ({isSignedIn, setData}) => {
     const handleSignOut = () => {
         const auth = getAuth();
         signOut(auth).then(() => {
-          // Sign-out successful.
-          const dataKeys = ['email', 'userName', 'displayName', 'password', 'confirmPassword'];
-          for (const key of dataKeys) {
-              const dataObject = {
-                  [key]: ''
-              }
-              setData(dataObject)
-              setToggleUserNav(false)
-          }
+            // Sign-out successful.
+            const dataKeys = ['email', 'userName', 'displayName', 'password', 'confirmPassword'];
+            for (const key of dataKeys) {
+                const dataObject = {
+                    [key]: ''
+                }
+                setData(dataObject)
+                setToggleUserNav(false)
+            }
         }).catch((error) => {
-          // An error happened.
+            // An error happened.
         });
     }
 
     return (
         <div className='header'>
-            {isSignedIn 
-            ? <div className='header-nav'>
-                {/* <Button 
+            <div className='container'>
+                {isSignedIn
+                    ? <div className='header-nav'>
+                        {/* <Button 
                     className='sign-out-btn'
                     text='Sign Out'
                     onClick={() => {handleSignOut()}}
                 /> */}
-                <img src={UserAvt} alt="" className="user-avt" onClick={() => {handleToggleUserNav()}}/>
-            </div>
-            : <div className='header-nav'>
+                        <img src={UserAvt} alt="" className="user-avt" onClick={() => { handleToggleUserNav() }} />
+                    </div>
+                    : <div className='header-nav'>
 
-            </div>
-            }
+                    </div>
+                }
 
-            {isSignedIn &&             
-            <div className={`${toggleUserNav && 'appear'} user-nav`}>
-                <LogOutBtn className='nav log-out' onClick={() => {handleSignOut()}}/>
-            </div>}
+                {isSignedIn &&
+                    <div className={`${toggleUserNav && 'appear'} user-nav`}>
+                        <LogOutBtn className='nav log-out' onClick={() => { handleSignOut() }} />
+                    </div>}
+            </div>
         </div>
     )
 }
 
-const mapStateToProps = ({isSignedIn}) => ({
+const mapStateToProps = ({ isSignedIn }) => ({
     isSignedIn: isSignedIn.isSignedIn
 })
 
--- src/pages/newsfeed/newsfeed.component.jsx
+++ src/pages/newsfeed/newsfeed.component.jsx
@@ -3,9 +3,29 @@ import './newsfeed.styles.scss';
 const NewsFeed = () => {
     return (
         <div className='newsfeed'>
+            <div className='container'>
+                <div className='left-con'>
+                    <div className='con stories'>
 
+                    </div>
+
+                    <div className='con posts'>
+                        
+                    </div>
+                </div>
+
+                <div className='right-con'>
+                    <div className='con user'>
+
+                    </div>
+
+                    <div className='con suggestions'>
+
+                    </div>
+                </div>
+            </div>
         </div>
-    )   
+    )
 }
 
 export default NewsFeed;
